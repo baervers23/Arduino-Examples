bb857b4fb0c74767275a8631843030fbb45d7490
baervers23 <58891174+baervers23@users.noreply.github.com>
2020-01-13 09:09:07 +0100

    Example to handle with EEPROM
    Example to handle with EEPROM data type byte (Value Range: -255 to +255). Im storing Configs here and i really dont need values more than 100~ but i dont like it this script was using double so i changed it to bytes which is definitly enough to store configurations like configurations.
    
    Original found on arduino forum posted from Halley, a guest.

---
+++ eeprom_ReadWrite_byte/eeprom_ReadWrite_byte.ino
@@ -0,0 +1,47 @@
+#include <EEPROM.h>
+#define BAUD_rate 115200
+
+byte noElem = 12; // number of bytes readed and written
+unsigned int baseAddr = 900; //Nane has 512, Uno hat 1024, Mega has 4096
+unsigned int n = 0; //temporary int for convert integer<->bytearray
+
+byte testInt[12] = { 10, 11, 22, 33, 44, 55, 66, 77, 88, 99, 46, 23};
+//byte testInt[12] = { -12.5, -10.00, -5.7, 0, 2.45, 2.90, 3.10, 4 , 5.6, 7.9, 5.5, 4};
+//byte testInt[12] = {};
+
+template <class T> int EEPROM_writeAnything(int ee, const T& value)
+{
+  const byte* p = (const byte*)(const void*)&value;
+  int i;
+  for (i = 0; i < sizeof(value); i++)
+    EEPROM.write(ee++, *p++);
+  return i;
+}
+
+template <class T> int EEPROM_readAnything(int ee, T& value)
+{
+  byte* p = (byte*)(void*)&value;
+  int i;
+  for (i = 0; i < sizeof(value); i++)
+    *p++ = EEPROM.read(ee++);
+  return i;
+}
+
+void setup() {
+Serial.begin(BAUD_rate);
+  // write data to eeprom
+  for (int i = 0; i <= noElem - 1; i++) {
+    n = EEPROM_writeAnything( (i * 2) + baseAddr, testInt[i]);
+  }
+
+  // read data back
+  for (int i = 0; i <= noElem - 1; i++) {
+    byte val;
+    int addr = (i * 2) + baseAddr;
+    n = EEPROM_readAnything( addr, val);
+    Serial.println(val);
+  }
+}
+
+void loop() {
+}
